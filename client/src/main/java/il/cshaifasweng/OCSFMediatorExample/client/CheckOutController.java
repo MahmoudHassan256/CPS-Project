c6ea334fbe95bd02323a7c733a2d996c5d72552f
mohamadkhaleel <102249794+mohamadkhaleel@users.noreply.github.com>
2023-01-15 13:15:45 +0200

    check out///

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CheckOutController.java
@@ -0,0 +1,15 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class CheckOutController {
+
+    @FXML
+    private AnchorPane CarLocation;
+
+    @FXML
+    private TextField licenseplatebox;
+
+}
